2a2c664b397ca013c4816fef9fc9df04040f5979
JesseAuta <jesse.auta@dci-student.org>
2026-03-19 11:40:32 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import SearchBar from "./components/SearchBar";
 import MapSection from "./components/MapSection";
 
-const API = "http://localhost:8000";
+const API = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
 
 interface Service {
   id: number;
