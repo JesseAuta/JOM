e01ef2df9e3f7cecee48d2788e405c4e0d87a6c1
Olena <olena.demchenko@dci-student.org>
2026-04-08 15:13:32 +0200

    dasboard issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang='de'>
       <body className='flex min-h-screen flex-col' suppressHydrationWarning>
         <Header />
-        <main className='flex-1'>{loading ? <Loader /> : children}</main>
+        <main className='flex-1'>{children}</main>
         <Footer />
       </body>
     </html>
